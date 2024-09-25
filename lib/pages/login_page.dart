430580c82ddeb610b51a7a42e462c6eff796e3b2
Ana Buhaescu <buhaescuana@yahoo.com>
2024-09-25 14:05:36 +0300

    email and password textfields created

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_textfield.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+   LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.lock_open_rounded,
+            size: 100,
+            color: Theme.of(context).colorScheme.inversePrimary,
+          ),
+          const SizedBox(height: 25),
+          Text(
+            'Food Delivery App',
+            style: TextStyle(
+              fontSize: 16,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+          ),
+          const SizedBox(height: 25),
+          MyTextfield(
+            controller: emailController,
+            hintText: 'Email',
+            obscureText: false,
+          ),
+          const SizedBox(height: 25),
+           MyTextfield(
+            controller: passwordController,
+            hintText: 'Password',
+            obscureText: true,
+          ),
+
+          const SizedBox(height: 25),
+        ],
+      ),
     );
   }
 }
